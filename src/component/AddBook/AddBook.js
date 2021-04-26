8743efbefb6e43ee4be05aab0ce3aa1536c72148
simone <sorzesimone1@gmail.com>
2021-04-26 21:37:39 +0200

    implementing addbook funcs

--- src/component/AddBook/AddBook.js
+++ src/component/AddBook/AddBook.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AddBook() {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [rating, setRating] = useState(5);
+
+  const addBookHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <form></form>
-    </div>
+    <>
+      <form>
+        <label htmlFor="title">Title</label>
+        <input
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label htmlFor="title">Author</label>
+        <input
+          name="author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <label htmlFor="title">Rating</label>
+        <input
+          name="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+          type="number"
+          min="1"
+          max="10"
+        />
+
+        <button onClick={addBookHandler}>Add Book</button>
+      </form>
+    </>
   );
 }
